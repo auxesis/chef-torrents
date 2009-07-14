e0bd9920282505f8d8a74a8d4b52118fc02542fd
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-07-14 15:48:20 +0000

    added nmap, telnet, lsof, and rsync packages

--- cookbooks/base_utils/recipes/default.rb
+++ cookbooks/base_utils/recipes/default.rb
@@ -1,3 +1,7 @@
-%w(man-db file mtr traceroute rar).each do |name|
+%w(man-db file mtr traceroute lsof rar rsync).each do |name|
+  package name
+end
+
+%w(telnet nmap).each do |name|
   package name
 end
